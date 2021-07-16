6fdf81cd80791dc855112560ca394c1fe3324431
adarsh112221 <adarsh.19b401007@abes.ac.in>
2021-07-16 09:44:19 +0530

    added slection filter

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,9 +1,13 @@
 import React from "react";
 import { useContent } from "../hooks/index";
+import selectionFilter from "../utils/selection-filter";
 export default function Browse() {
   //we need the series and we need the slides
   const { series } = useContent("series");
+  const { films } = useContent("films");
   console.log(series);
+  const slides = selectionFilter({ series, films });
+  console.log(slides);
   //pass it to the browse container
   return <p>hello from the browse!</p>;
 }
