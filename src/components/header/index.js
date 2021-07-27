5cf7b5f3f622fdc334974f93bc9200db27b5f5d7
adarsh112221 <adarsh.19b401007@abes.ac.in>
2021-07-27 15:19:52 +0530

    creating Header-Section

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,22 +1,59 @@
-import React from 'react'
-import {Link as ReactRouterLink} from 'react-router-dom'
-import {Background,Container,ButtonLink,Logo} from './styles/header'
-export default function Header({bg=true,children,...restProps})
-{
-    return bg?(<Background {...restProps}>{children}</Background>):(children);
+import React from "react";
+import { Link as ReactRouterLink } from "react-router-dom";
+import {
+  Background,
+  Container,
+  Feature,
+  Text,
+  ButtonLink,
+  FeatureCallOut,
+  Logo,
+  Group,
+  Link,
+  Picture,
+  Profile,
+  Dropdown,
+} from "./styles/header";
+export default function Header({ bg = true, children, ...restProps }) {
+  return bg ? <Background {...restProps}>{children}</Background> : children;
 }
-Header.Frame=function HeaderFrame({children,...restProps})
-{
-    return <Container {...restProps}>{children}</Container>
-}
-Header.ButtonLink=function HeaderButtonLink({children,...restProps})
-{
-    return <ButtonLink {...restProps}>{children}</ButtonLink>
-}
-Header.Logo=function HeaderLogo({to,...restProps})
-{
-return <ReactRouterLink to={to}>
-    <Logo {...restProps}/>
-</ReactRouterLink>
-
-}
+Header.Feature = function HeaderFeature({ children, ...restProps }) {
+  return <Feature {...restProps}>{children}</Feature>;
+};
+Header.Frame = function HeaderFrame({ children, ...restProps }) {
+  return <Container {...restProps}>{children}</Container>;
+};
+Header.Group = function HeaderGroup({ children, ...restProps }) {
+  return <Group {...restProps}>{children}</Group>;
+};
+Header.FeatureCallOut = function HeaderFeatureCallOut({
+  children,
+  ...restProps
+}) {
+  return <FeatureCallOut {...restProps}>{children}</FeatureCallOut>;
+};
+Header.Profile = function HeaderProfile({ children, ...restProps }) {
+  return <Profile {...restProps}>{children}</Profile>;
+};
+Header.Dropdown = function HeaderDropdown({ children, ...restProps }) {
+  return <Dropdown {...restProps}>{children}</Dropdown>;
+};
+Header.Text = function HeaderText({ children, ...restProps }) {
+  return <Text {...restProps}>{children}</Text>;
+};
+Header.Picture = function HeaderPicture({ src, ...restProps }) {
+  return <Picture {...restProps} src={`images/users/${src}.png`} />;
+};
+Header.TextLink = function HeaderTextLink({ children, ...restProps }) {
+  return <Link {...restProps}>{children}</Link>;
+};
+Header.ButtonLink = function HeaderButtonLink({ children, ...restProps }) {
+  return <ButtonLink {...restProps}>{children}</ButtonLink>;
+};
+Header.Logo = function HeaderLogo({ to, ...restProps }) {
+  return (
+    <ReactRouterLink to={to}>
+      <Logo {...restProps} />
+    </ReactRouterLink>
+  );
+};
--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,8 +1,9 @@
 import React, { useContext, useState, useEffect } from "react";
 import { SelectionProfileContainer } from "./profiles";
 import { FireBaseContext } from "../context/firebase";
-import { Profile } from "../components/header/styles/header";
-import { Loading } from "../components";
+import { Header, Loading } from "../components";
+import * as ROUTES from "../constants/routes";
+import Logo from "../logo.svg";
 export function BrowseContainer({ slides }) {
   const [profile, setProfile] = useState({});
   const [loading, setLoading] = useState(true);
@@ -14,9 +15,39 @@ export function BrowseContainer({ slides }) {
     }, 3000);
   }, [profile.displayName]);
   return profile.displayName ? (
-    loading ? (
-      <Loading src={user.photoURL} />
-    ) : <Loading.ReleaseBody/>
+    <>
+      {loading ? <Loading src={user.photoURL} /> : <Loading.ReleaseBody />}
+      <Header src="joker1">
+        <Header.Frame>
+          <Header.Group>
+            <Header.Logo to={ROUTES.HOME} alt="netflix" src={Logo} />
+            <Header.TextLink>Series</Header.TextLink>
+            <Header.TextLink>Films</Header.TextLink>
+          </Header.Group>
+          <Header.Group>
+            <Header.Profile>
+              <Header.Picture  src={user.photoURL}/>
+            <Header.Dropdown>
+              <Header.Group>
+                <Header.Picture src={user.photoURL} />
+                <Header.TextLink>{user.displayName}</Header.TextLink>
+              </Header.Group>
+            </Header.Dropdown>
+            </Header.Profile>
+          </Header.Group>
+        </Header.Frame>
+        <Header.Feature>
+          <Header.FeatureCallOut>Watch Joker Now</Header.FeatureCallOut>
+          <Header.Text>
+            Forever alone in a crowd, failed comedian Arthur Fleck seeks
+            connection as he walks the streets of Gotham City. Arthur wears two
+            masks -- the one he paints for his day job as a clown, and the guise
+            he projects in a futile attempt to feel like he's part of the world
+            around him.
+          </Header.Text>
+        </Header.Feature>
+      </Header>
+    </>
   ) : (
     <SelectionProfileContainer user={user} setProfile={setProfile} />
   );
